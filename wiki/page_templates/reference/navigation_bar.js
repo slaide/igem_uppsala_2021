e0c633cd7f63eb3b5bd0ba5e4ea92dab7d337c42
slaide <patrick.hennig@fau.de>
2021-10-06 22:08:36 +0200

    improved layout of hp page switcher headers, hopefully fixed navigation bar text layout, added some padding at bottom of all pages

--- wiki/page_templates/reference/navigation_bar.js
+++ wiki/page_templates/reference/navigation_bar.js
@@ -1,27 +1,25 @@
-<script purpose="internal_open_close_navbar">
-            function open_navbar(){
-                document.getElementById("navigation_bar").classList.add("fully_open")
-                document.getElementById("pagecontent").classList.add("blurred_navigation_bar_open")
-                //this is broken. blurring the main element makes the scrolly things lose their position setting
-                for(element of document.getElementsByClassName("scroll_thing")){
-                    element.style.setProperty("filter","blur(15px)")
-                }
-            }
-            function close_navbar(){
-                document.getElementById("navigation_bar").classList.remove("fully_open")
-                document.getElementById("pagecontent").classList.remove("blurred_navigation_bar_open")
-                //this is broken. blurring the main element makes the scrolly things lose their position setting
-                for(element of document.getElementsByClassName("scroll_thing")){
-                    element.style.setProperty("filter","initial")
-                }
-            }
+function open_navbar(){
+    document.getElementById("navigation_bar").classList.add("fully_open")
+    document.getElementById("pagecontent").classList.add("blurred_navigation_bar_open")
+    //this is broken. blurring the main element makes the scrolly things lose their position setting
+    for(element of document.getElementsByClassName("scroll_thing")){
+        element.style.setProperty("filter","blur(15px)")
+    }
+}
+function close_navbar(){
+    document.getElementById("navigation_bar").classList.remove("fully_open")
+    document.getElementById("pagecontent").classList.remove("blurred_navigation_bar_open")
+    //this is broken. blurring the main element makes the scrolly things lose their position setting
+    for(element of document.getElementsByClassName("scroll_thing")){
+        element.style.setProperty("filter","initial")
+    }
+}
 
-            /* open/close navigation bar while scrolling */
-            window.addEventListener("wheel",(ev)=>{
-                if(ev.deltaY>0){
-                    document.getElementById("dropdown-container").classList.remove("scrolled_down")
-                }else{
-                    document.getElementById("dropdown-container").classList.add("scrolled_down")
-                }
-            })
-        </script>
+/* open/close navigation bar while scrolling */
+window.addEventListener("wheel",(ev)=>{
+    if(ev.deltaY>0){
+        document.getElementById("dropdown-container").classList.remove("scrolled_down")
+    }else{
+        document.getElementById("dropdown-container").classList.add("scrolled_down")
+    }
+})
