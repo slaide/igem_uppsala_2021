b20178e3ed6bf2762951892b6be1fcda7a8b94e6
slaide <patrick_hennig@fastmail.com>
2021-10-08 23:23:25 +0200

    updated webinar schedule

---
+++ wiki/page_templates/education/schedule.js
@@ -0,0 +1,107 @@
+
+/*
+schedule day 1 (Aug 29)
+10:15: OPENING REMARKS
+10:30 INSIGHT INTO IGEM PROJECTS: 
+UPPSALA, MAASTRICHT, WAGENINGEN, GRONINGEN
+11:00 RUTH PURCELL PAPERS TO PATENTS: HOW CELLULAR AGRICULTURE LED MEFROM ACADEMIA TO INDUSTRY
+11:30 OLA THOMSSON 
+SUSTAINABLE FARMING: A COMPLEX THING
+12:00 TOMAS LINDER BEYOND AGRICULTURE: HOW EDIBLE MICROORGANISMS CANREVOLUTIONIZE THE GLOBAL FOOD SYSTEM
+12:30 CLOSING REMARKS FOR DAY 1
+13:00: SOPHYA INFORMAL NETWORKING STARTS
+*/
+/*
+Day 2 (Aug 30) schedule (GMT+2):
+
+16:45 OPENING REMARKS
+17:00 MARK POST: 
+PAVING THE WAY TOWARDS A SUSTAINABLE MEAT INDUSTRY WITHCULTIVATED MEAT
+17:30 SARA PETERSSON INTELLECTUAL PROPERTY IN AGRICULTURE
+18:00 MICHAEL SAAD 
+PERSPECTIVES FOR CELLULAR AQUACULTURE
+18:30 - 19:00 BREAK
+19:00 PAUL SHAPIRO FERMENTING THE FUTURE: TURNING MICROBES INTO MEAT
+19:30 IRFAN TAHIR
+HOW CAN WE MAKE CELLULAR AGRICULTURE MORE ACCESSIBLE FORFARMERS?
+20:00 ABI ASPEN GLENCROSS
+FROM LAB GROWN MEAT TO THE FIELD OF WHEAT - AND BACK AGAIN
+20:30 CLOSING REMARKS
+21:00 SOPHYA INFORMAL NETWORKING STARTS
+*/
+schedule_day1=[
+    {
+        "speaker":"iGEM Projects",
+        "topic":"INSIGHT INTO IGEM PROJECTS: Uppsala, Maastricht, Wageningen, Groningen",
+        "image":"",
+        "time":["10.30"]
+    },
+    {
+        "speaker":"Ruth Purcell",
+        "topic":"PAPERS TO PATENTS: How Cellular Agriculture led me from Academia to Industry",
+        "image":"Ruth_Purcell.jpeg",
+        time:["11.00"]
+    },
+    {
+        "speaker":"Ola Thomsson",
+        "topic":"SUSTAINABLE FARMING: a Complex Thing",
+        "image":"Ola_Thomsson.jpeg",
+        time:["11.30"]
+    },
+    {
+        "speaker":"Tomas Linder",
+        "topic":"BEYOND AGRICULTURE: How Edible Microorganisms Can Revolutionize the Global Food System",
+        "image":"Tomas_Linder.jpeg",
+        time:["12.00"]
+    },
+    {
+        speaker:"Sophya Virtual World",
+        topic:"Informal networking and socialization starts",
+        "image":"Sophya.png",
+        time:["12.45"]
+    },
+]
+schedule_day2=[
+    {
+        "speaker":"Mark Post",
+        "topic":"Paving the Way Towards a Sustainable Meat Industry with Cultivated Meat",
+        "image":"Mark_Post.jpeg",
+        time:["17.00"]
+    },
+    {
+        "speaker":"Sara Petersson",
+        "topic":"Intellectual Property in Agriculture",
+        "image":"Sara_Petersson.jpeg",
+        time:["17.30"]
+    },
+    {
+        "speaker":"Michael Saad",
+        "topic":"Perspectives for Cellular Aquaculture",
+        "image":"Michael_Saad.png",
+        time:["18.00"]
+    },
+    {
+        "Speaker":"Paul Shapiro",
+        "topic":"FERMENTING THE FUTURE: Turning Microbes into Meat",
+        "image":"Paul_Shapiro.jpeg",
+        time:["19.00"]
+    },
+    {
+        "Speaker":"Irfan Tahir",
+        "topic":"How Can We Make Cellular Agriculture More Accessible For Farmers?",
+        "image":"Irfan_Tahir.jpeg",
+        time:["19.30"]
+    },
+    {
+        "speaker":"Abi Aspen Glencross",
+        "topic":"From Lab Grown Meat to field of Wheat - and back again",
+        "image":"Abi_Glencross.png",
+        time:["20.00"]
+    },
+    {
+        speaker:"Sophya Virtual World",
+        topic:"Informal networking and socialization starts",
+        "image":"Sophya.png",
+        time:["21.00"]
+    },
+]
