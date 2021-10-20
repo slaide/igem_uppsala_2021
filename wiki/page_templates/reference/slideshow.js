883635a8c47faf64f40c2fb9fc1444253eee9eea
Patrick Hennig <patrick_hennig@fastmail.com>
2021-10-20 18:21:02 +0200

    fixed slideshow layout and functionality

--- wiki/page_templates/reference/slideshow.js
+++ wiki/page_templates/reference/slideshow.js
@@ -1,7 +1,8 @@
 function nextSlide(slideshow_id) {
     let slideshow_element=document.getElementById(slideshow_id)
-    let current_slide_id=slideshow_element.getAttribute("current_slide_id")
-    let number_of_slides=slideshow_element.getAttribute("number_of_slides")
+    let current_slide_id=Number(slideshow_element.getAttribute("current_slide_id"))
+    console.log("csi"+current_slide_id)
+    let number_of_slides=Number(slideshow_element.getAttribute("number_of_slides"))
 
     let new_slide_id=current_slide_id+1
 
@@ -10,15 +11,19 @@ function nextSlide(slideshow_id) {
     }
 
     let old_slide_element=slideshow_element.children[current_slide_id]
+    console.log("ose")
+    console.log(old_slide_element)
     let new_slide_element=slideshow_element.children[new_slide_id]
 
     showSlide(slideshow_element,old_slide_element,new_slide_element);
+
+    slideshow_element.setAttribute("current_slide_id",""+new_slide_id)
 }
 
 function previousSlide(slideshow_id) {
     let slideshow_element=document.getElementById(slideshow_id)
-    let current_slide_id=slideshow_element.getAttribute("current_slide_id")
-    let number_of_slides=slideshow_element.getAttribute("number_of_slides")
+    let current_slide_id=Number(slideshow_element.getAttribute("current_slide_id"))
+    let number_of_slides=Number(slideshow_element.getAttribute("number_of_slides"))
 
     let new_slide_id=current_slide_id-1
 
@@ -30,18 +35,20 @@ function previousSlide(slideshow_id) {
     let new_slide_element=slideshow_element.children[new_slide_id]
 
     showSlide(slideshow_element,old_slide_element,new_slide_element);
+
+    slideshow_element.setAttribute("current_slide_id",""+new_slide_id)
 }
 
 function showSlide(_target_element,old_slide_element,new_slide_element) {
-    new_slide_element.style.setProperty("display","none");
+    new_slide_element.style.setProperty("display","block");
 
     if(old_slide_element){
-       old_slide_element.style.setProperty("display","block");
+       old_slide_element.style.setProperty("display","none");
     }
 }
 
 function init_slideshows(){
-    for(slideshow_container of document.getElementsByClassName("slideshow-container")){
+    for(let slideshow_container of document.getElementsByClassName("slideshow-container")){
         if(!slideshow_container.id){
             window.alert("a slideshow container does not have an id! (but should have one)")
             console.log("add id to "+slideshow_container)
@@ -49,27 +56,37 @@ function init_slideshows(){
 
         let number_of_slides=0
         for(child of slideshow_container.children){
-            if(!child.classList.contains("mySlide")){
+            if(!child.classList.contains("mySlide") && !child.tagName=="A"){
                 window.alert("slideshow "+slideshow_container.id+"contains a child that is not a slide(missing mySlide class)!")
                 console.log("error in "+slideshow_container)
             }
             number_of_slides+=1
         }
-        slideshow_container.setAttribute("number_of_slides")=number_of_slides
+        slideshow_container.setAttribute("number_of_slides",number_of_slides)
 
         let first_slide=slideshow_container.children[0]
 
         showSlide(slideshow_container,null,first_slide)
 
         let next_button=document.createElement("a")
-        next_button.outerHTML=`
-            <a class="prev" onclick="nextSlide('${slideshow_container.id}')">❮</a>
-        `
+        next_button.classList.add("next")
+        next_button_text=document.createElement("span")
+        next_button_text.innerText="❯"
+        next_button.appendChild(next_button_text)
+
+        next_button.setAttribute("onclick",`nextSlide("${slideshow_container.id}")`)
+
         let previous_button=document.createElement("a")
-        previous_button.outerHTML=`
-            <a class="next" onclick="previousSlide('${slideshow_container.id}')">❯</a>
-        `
+        previous_button.classList.add("prev")
+        previous_button_text=document.createElement("span")
+        previous_button_text.innerText="❮"
+        previous_button.appendChild(previous_button_text)
+
+        previous_button.setAttribute("onclick",`previousSlide("${slideshow_container.id}")`)
+
         slideshow_container.appendChild(next_button)
         slideshow_container.appendChild(previous_button)
+
+        slideshow_container.setAttribute("current_slide_id","0")
     }
 }
